f1d8fe2b17a44e53e78fffbc91e7a5a5965beba4
Ali Muhammad <alimuhammadfc5@gmail.com>
2022-12-25 01:37:00 +0500

    fix error

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,44 +1,8 @@
-import { ClientSideSuspense } from "@liveblocks/react";
-import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
-import { RoomProvider } from "../liveblocks.config";
+import { NextPage } from "next";
 import Room from "./components/room";
 
-type Props = {
-	id: string;
-};
-
-const TopRoom: NextPage<Props> = ({ id }) => {
-	return (
-		<RoomProvider id={id} initialPresence={{ cursor: { x: 0, y: 0 } }}>
-			<ClientSideSuspense
-				fallback={
-					<div className="w-full flex justify-center mt-28">
-						Loading...
-					</div>
-				}
-			>
-				{() => <Room />}
-			</ClientSideSuspense>
-		</RoomProvider>
-	);
-};
-
-export const getServerSideProps: GetServerSideProps = async (
-	ctx: GetServerSidePropsContext
-) => {
-	const id = ctx.params?.id as string;
-
-	if (!id) {
-		return {
-			notFound: true,
-		};
-	}
-
-	return {
-		props: {
-			id,
-		},
-	};
+const TopRoom: NextPage = () => {
+	return <Room />;
 };
 
 export default TopRoom;
